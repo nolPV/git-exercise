ada441a30eb07830bfc5be22185b5c182696ba27
nolPV <nolnetlog@gmail.com>
2020-03-11 12:39:13 +0700

    Add function division of 2 number

--- functions.php
+++ functions.php
@@ -9,4 +9,12 @@
 
 	function convertCToK($c) {
 		return $c + 273.15; 
-	} 
+	}
+
+	function division-of-2-number($a, $b) {
+ 		if($b!=0) {
+			return $a / $b;
+		} else {
+			return "Divider could not be 0.";
+		}
+	}
